86070e512972c49348c1a6141bda3b3820bf4ed6
lilian8950 <liliankisulu9@gmail.com>
2025-12-03 10:29:35 +0300

    Initial commit

---
+++ folder_one/example.py/example1.py
@@ -0,0 +1,4 @@
+# Simple Python example
+numbers = [1, 2, 3, 4, 5]
+square = [n**2 for n in numbers]
+print("Squares:", square)
---
+++ folder_two/example.R
@@ -0,0 +1,4 @@
+# Simple R example
+numbers <- c(1, 2, 3, 4, 5)
+square <- numbers^2
+print(square)
